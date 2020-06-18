553b0afcc8270541febaa15b90ed4f91fb36a2db
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-06-18 16:09:22 +0530

    Added context

---
+++ contextapp/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
+
+
+function App() {
+  return (
+    <div className="App">
+    <ThemeContextProvider>
+      <AuthContextProvider>
+        <Navbar/>
+        <BookList/>
+        <ThemeToggle />
+      </AuthContextProvider>
+    </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ contextapp/src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends Component {
+    static contextType = ThemeContext;
+    render() {
+        const {isLightTheme, light, dark} = this.context;
+        const theme = isLightTheme ? light: dark;
+        return (
+            <div className='book-list' style={{color:theme.syntax, background:theme.bg}}>
+                <ul>
+                    <li style={{background:theme.ui}}>A Clash of Kings</li>
+                    <li style={{background:theme.ui}}>The Winds of Winter</li>
+                    <li style={{background:theme.ui}}>A Game of Thrones</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BookList;
