9beb1246f242ce1b17771cfd843be19a7bb000da
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-06-27 00:33:21 +0530

    Added Reducers

---
+++ booklistreducer/src/reducers/bookReducer.js
@@ -0,0 +1,18 @@
+import uuid from 'uuid/dist/v1';
+
+export const bookReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_BOOK':
+            return [...state, {
+                title: action.book.title,
+                author: action.book.author,
+                id : uuid()
+            }]
+        case 'REMOVE_BOOK':
+            return state.filter(book => book.id !== action.id);
+        default:
+            return state;
+    }
+}
+
+export default bookReducer;
